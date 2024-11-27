034daa4f6526c718af7c790eed2015e19655df04
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-11-27 12:30:15 +0100

    "Deleted SVG files, font files, and updated CSS and TSX files"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import React from "react";
-
-import {
-  ProgressCircleRing,
-  ProgressCircleRoot,
-} from "@/components/ui/progress-circle";
 
 export default function Home() {
   return (
-    <main className="flex w-screen h-screen justify-center items-center">
-      <ProgressCircleRoot value={null}>
-        <ProgressCircleRing />
-      </ProgressCircleRoot>
+    <main className=" text-black">
+      helloworld
     </main>
   );
 }
