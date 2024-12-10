1e29b2474c5a17504b38cb9d3a836009fe3ec97f
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-12-10 17:20:39 +0100

    `feat: updated AuthWithGoogle component and GoogleAuth utility to handle user authentication with Google`

--- src/components/Auth/AuthWithGoogle.tsx
+++ src/components/Auth/AuthWithGoogle.tsx
@@ -14,6 +14,23 @@ interface AuthWithGoogleProps {
   Action: "signIn" | "signOut";
 }
 
+/**
+ * A functional component that handles user authentication with Google.
+ *
+ * @param Action - Determines the action to be performed. It can be either "signIn" or "signOut".
+ *
+ * @returns A form with a button to initiate the specified action with Google.
+ *
+ * @remarks
+ * This component uses Next.js' `useRouter` hook to navigate between different routes.
+ * It also utilizes the `useState` hook to manage the user session state.
+ * The `useEffect` hook is used to fetch the user session and navigate to the appropriate route based on the session status.
+ * The `handleSubmit` function is called when the form is submitted. It prevents the default form submission behavior,
+ * and then calls the appropriate authentication function based on the `Action` prop.
+ * If an error occurs during the authentication process, it is logged to the console.
+ */
+
+ 
 export default function AuthWithGoogle({ Action }: AuthWithGoogleProps) {
   const router = useRouter();
   const [userSession, setSession] = useState<Session | null>(null);
@@ -23,8 +40,7 @@ export default function AuthWithGoogle({ Action }: AuthWithGoogleProps) {
       const sessionData = await getSession();
       setSession(sessionData);
       if (sessionData) {
-        // router.push(routeLinks.chooseInterest);
-        console.log(sessionData.user)
+        router.push(routeLinks.chooseInterest);
       } else {
         router.push(routeLinks.signup);
       }
@@ -62,3 +78,4 @@ export default function AuthWithGoogle({ Action }: AuthWithGoogleProps) {
     </form>
   );
 }
+
--- src/utils/AuthProviders/GoogleAuth.ts
+++ src/utils/AuthProviders/GoogleAuth.ts
@@ -4,7 +4,6 @@ import { signIn, signOut } from '@/auth';
 
 export async function signInWithGoogle() {
     await signIn("google");
-    // Do not fetch the session here; handle it on the client side
 }
 
 export async function signOutWithGoogle() {
