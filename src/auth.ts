d8d9ab704173faffbca489ddd054963c22be3756
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-12-11 16:47:15 +0100

    `Added CredentialsProvider to NextAuth configuration`

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [Google],
+    providers: [Google,
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {}
+            }
+        })],
 })
