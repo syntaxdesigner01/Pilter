e0a582539753560e41c4a2cdbad24b0718f2415c
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-12-14 12:23:58 +0100

    feat: added generateId file

---
+++ lib/generateId.ts
@@ -0,0 +1,13 @@
+function generateId(length = 8) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+
+    return result;
+}
+
+export default generateId
