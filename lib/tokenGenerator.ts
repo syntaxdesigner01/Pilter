402cb4d8304b15ff43cb9b6cbc83db99b01a8e93
syntaxdesigner01 <syntaxdesigners@gmail.com>
2024-12-23 00:44:10 +0100

    Add JWT token generation and verification functions; implement user authentication support

---
+++ lib/tokenGenerator.ts
@@ -0,0 +1,23 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
+
+
+interface User {
+    email: string;
+    name: string;
+}
+
+export const sign_Jwt_Token = (user: User): string => {
+    return jwt.sign({ name:user.name, email: user.email }, JWT_SECRET, { expiresIn: '30d' }); 
+};
+
+export const verify_Jwt_Token = (token: string): JwtPayload | string => {
+    try {
+        return jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+        console.error("Token verification failed:", error);
+        throw new Error("Invalid token");
+    }
+};
