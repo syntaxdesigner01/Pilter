56b4d492d5d9b15af5b083ffb5344a5d7df29349
syntaxdesigner01 <syntaxdesigners@gmail.com>
2025-01-14 11:02:34 +0100

    feat: add pagination to AsideLeftComponent and adjust Divider width for improved layout

--- src/components/Protected/KittyAiPage/AsideLeftComponent.tsx
+++ src/components/Protected/KittyAiPage/AsideLeftComponent.tsx
@@ -1,7 +1,15 @@
 'use client'
 import { Seasons } from '@/utils/data';
-import { Input } from '@chakra-ui/react';
+import { Input, Stack } from '@chakra-ui/react';
 import React, { useState } from 'react'
+import { HStack } from "@chakra-ui/react";
+import {
+  PaginationItems,
+  PaginationNextTrigger,
+  PaginationPrevTrigger,
+  PaginationRoot,
+} from "@/components/ui/pagination";
+
 
 
 export default function AsideLeftComponent() {
@@ -9,7 +17,7 @@ export default function AsideLeftComponent() {
   const [keyword,setKeyword] = useState<string>('')
   const [dataset,setDataset] = useState<string[][]>([])
 
-
+const [page, setPage] = useState(1);
 
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -31,7 +39,7 @@ export default function AsideLeftComponent() {
   }
 
   return (
-    <section className="w-full relative left-[-4em] h-screen overflow-y-auto">
+    <section className="w-full relative left-[-4em] h-screen overflow-y-scroll scrollbar-hide">
       <h1 className="text-left text-sm font-semibold">Prompt suggestion: </h1>
 
       <form className="mt-2" onSubmit={(e) => handleSubmit(e)}>
@@ -48,7 +56,7 @@ export default function AsideLeftComponent() {
       </p>
       <hr />
 
-      <section className='pb-20'>
+      {/* <section className='pb-20'>
         {dataset.length > 0 &&
           dataset.map((seasonPrompts, index) => {
             return (
@@ -68,7 +76,45 @@ export default function AsideLeftComponent() {
               </section>
             );
           })}
-      </section>
+      </section> */}
+
+      <Stack>
+        <Stack gap={4}>
+          {dataset.length > 0 &&
+            dataset.slice((page - 1) * 3, page * 3).map((seasonPrompts, index) => {
+              return (
+                <section
+                  key={index}
+                  className="mt-4 border-2 border-black  rounded-xl  leading-relaxed tracking-wider  text-sm"
+                >
+                  <section className="flex justify-end items-center mb-[0.7px] rounded-tr-xl">
+                    <button className="bg-black text-white px-4">
+                      Copy text
+                    </button>
+                  </section>
+
+                  <section className="bg-dark text-white p-3">
+                    {seasonPrompts}
+                  </section>
+                </section>
+              );
+            })}
+        </Stack>
+        {dataset.length > 0 && (
+          <PaginationRoot
+            count={5}
+            pageSize={1}
+            page={page}
+            onPageChange={(e) => setPage(e.page)}
+          >
+            <HStack>
+              <PaginationPrevTrigger />
+              <PaginationItems />
+              <PaginationNextTrigger />
+            </HStack>
+          </PaginationRoot>
+        )}
+      </Stack>
     </section>
   );
 }
--- src/components/Protected/KittyAiPage/Divider.tsx
+++ src/components/Protected/KittyAiPage/Divider.tsx
@@ -5,7 +5,7 @@ export default function Divider() {
   return (
     <section className="w-full flex items-center pt-10">
       <section className="w-full flex justify-evenly items-center shadow-lg z-50 ">
-        <section className="w-[23vw] h-screen border-r-4 border-black ">
+        <section className="w-[25vw] h-screen  ">
           <AsideLeftComponent/>
         </section>
 
